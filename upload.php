fe0c7acd770ba83ad5fd48e37e432435b8be065f
Charles <charlesbel30@gmail.com>
2020-05-30 17:23:07 +0200

    Update to API V3

--- upload.php
+++ upload.php
@@ -16,20 +16,8 @@ $response = json_decode($response, true);
 $token = $response["access_token"];
 
 
-$url = "https://www.googleapis.com/upload/drive/v2/files?uploadType=multipart";
-   
-$headers = array(
-    "Content-Type: multipart/related; boundary=287032381131322",
-    "Authorization: Bearer " . $token
-    );
-   
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-curl_setopt($ch, CURLOPT_BINARYTRANSFER, TRUE);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_POST, true);
+$url = "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&supportsAllDrives=true";
+
 $postdata = '
 --287032381131322
 Content-Type: application/json
@@ -44,6 +32,22 @@ Content-Type: ' . $_FILES['file']['type'] . '
 ' . file_get_contents($_FILES['file']['tmp_name']) . '
 --287032381131322--
 ';
+
+$lenght = strlen($postdata);
+
+$headers = array(
+    "Content-Type: multipart/related; boundary=287032381131322",
+    "Authorization: Bearer " . $token,
+    "Content-Length: " . $lenght
+    );
+   
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_BINARYTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
    
 $output = curl_exec($ch);
